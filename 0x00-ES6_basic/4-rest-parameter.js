75da0071410025d43dbcc34d431699571faddf7c
YomnaMohsen <ymn_mohsen@yahoo.com>
2024-08-06 22:46:53 +0300

    t 4

--- 0x00-ES6_basic/4-rest-parameter.js
+++ 0x00-ES6_basic/4-rest-parameter.js
@@ -1,7 +1,3 @@
 export default function returnHowManyArguments(...theargs) {
-  let counter = 0;
-  for (const arg of theargs) {
-    counter += 1;
-  }
-  return counter;
+  return theargs.length;
 }
