634f03380889d00df3857b865563543b1cf52184
YomnaMohsen <ymn_mohsen@yahoo.com>
2024-08-08 19:13:11 +0300

    t 3

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,8 +1,9 @@
 export default function handleResponseFromAPI(promise){
     return promise
-    .then(()=>({status: 200, body:"success"}))
+    .then(()=>{
+        const data = {status: 200, body:"success"};
+    }
+)
     .catch(()=> new Error())
     .finally(()=>console.log("Got a response from the API"));
 }
-const promise = Promise.resolve();
-handleResponseFromAPI(promise);
