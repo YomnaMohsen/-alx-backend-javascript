d6285258ffb42e51a94365860ffb1cdfa0f53ebb
YomnaMohsen <ymn_mohsen@yahoo.com>
2024-08-08 19:26:34 +0300

    t 4

--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -1,2 +1,7 @@
 export default function signUpUser(firstName, lastName) {
+    return new Promise((resolve, reject) => {
+        if (firstName && lastName){
+            resolve({firstName: firstName,lastName: lastName});
+        }
+    })
 }
