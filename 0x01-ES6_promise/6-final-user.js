4bf27544eaa87ecac13192e46d9fb9a0346f6681
YomnaMohsen <ymn_mohsen@yahoo.com>
2024-08-08 22:38:58 +0300

    t 6

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,14 @@
+import signUpUser from "./4-user-promise";
+import uploadPhoto from "./5-photo-reject";
+
+export default function handleProfileSignup(firstName, lastName, fileName){
+    return Promise.allSettled([signUpUser(firstName, lastName),uploadPhoto(fileName)])
+    .then((values) => {
+         for (const val of values){
+            if (val.status === "rejected"){
+                val.value = val.reason;
+                delete val.reason;
+            }
+         }   
+    });
+}
