28fb44facdf436574f5a1c0d4cbafc69b5ef999a
YomnaMohsen <ymn_mohsen@yahoo.com>
2024-08-09 19:57:53 +0300

    t 8 &9

---
+++ 0x01-ES6_promise/9-try.js
@@ -0,0 +1,16 @@
+import divideFunction from './8-try';
+export default function guardrail(mathFunction){
+    const queue = [];
+    try{
+        queue.push(mathFunction());
+    }
+    catch(error){
+        queue.push(error.toString())
+    }
+    finally{
+        queue.push('Guardrail was processed')
+    }
+    return queue;
+}
+console.log(guardrail(() =>(divideFunction(10, 2))));
+console.log(guardrail(() => {divideFunction(10, 0)}));
