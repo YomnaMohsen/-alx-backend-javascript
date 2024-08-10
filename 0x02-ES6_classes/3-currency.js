73953f0cd006bc82cacf30e308f155d56131c363
Yomna Mohsen <ymn_mohsen@yahoo.com>
2024-08-10 21:20:20 +0300

    t 3

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,12 +1,12 @@
 export default class Currency{
-    constructor(code, name){
-        if (typeof code !== "string"){
-            throw new TypeError("Code must be a string");
-        }
-        this._code = code;
-        if (typeof name !== "string"){
-            throw new TypeError("Name must be a string");
-        }
+  constructor(code, name){
+    if (typeof code !== "string"){
+        throw new TypeError("Code must be a string");
+    }
+    this._code = code;
+    if (typeof name !== "string"){
+        throw new TypeError("Name must be a string");
+    }
         this._name = name;
     }
 
@@ -14,15 +14,16 @@ export default class Currency{
     {
         return this._name;
     }
-    get code(){
-        return this.code;
-    }
     set name(new_name)
     {   
         if (typeof new_name !== "string"){
             throw new TypeError("Name must be a string");
         }
         this._name = new_name;
+    }
+    get code()
+    {
+        return this._code;
     }
     set code(new_code)
     {   
