10acec3d683c4f4dff7f545a2ef978fba6574b70
Yomna Mohsen <ymn_mohsen@yahoo.com>
2024-08-10 23:38:13 +0300

    t 4

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -9,14 +9,3 @@ export default class Building{
         return this._sqft;
     }
 }
-const b = new Building(100);
-console.log(b);
-
-class TestBuilding extends Building {}
-
-try {
-    new TestBuilding(200)
-}
-catch(err) {
-    console.log(err);
-}
