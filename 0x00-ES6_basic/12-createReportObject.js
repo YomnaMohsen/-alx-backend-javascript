5393eb8281d979e8773342cc93eee0841f7ad80a
Yomna Mohsen <ymn_mohsen@yahoo.com>
2024-09-30 23:21:20 +0300

    t 12

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -8,11 +8,4 @@ export default function createReportObject(employeesList) {
     },
   };
 }
-const employees = {
-  ...createEmployeesObject('engineering', ['Bob', 'Jane']),
-  ...createEmployeesObject('marketing', ['Sylvie']),
-};
 
-const report = createReportObject(employees);
-console.log(report.allEmployees);
-console.log(report.getNumberOfDepartments(report.allEmployees));
