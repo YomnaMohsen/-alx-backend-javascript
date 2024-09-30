80e357a488cd4948e2749c57d5095a1dcccd4d38
Yomna Mohsen <ymn_mohsen@yahoo.com>
2024-09-30 23:45:50 +0300

    promises

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,11 @@
+export default function getResponseFromAPI() {
+  const p = new Promise(((resolve, reject) => {
+    const success = true;
+    if (success) {
+      resolve(success);
+    } else {
+      reject('failed');
+    }
+  }));
+  return p;
+}
---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,8 @@
+export default function handleResponseFromAPI(promise) {
+  return promise
+    .then(() => {
+      const data = { status: 200, body: 'success' };
+    })
+    .catch(() => new Error())
+    .finally(() => console.log('Got a response from the API'));
+}
