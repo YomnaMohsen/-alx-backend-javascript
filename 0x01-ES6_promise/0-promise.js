fc72a56331267ff06044c0415b541fd66e5eccaa
Yomna Mohsen <ymn_mohsen@yahoo.com>
2024-10-01 19:15:17 +0300

    t0

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -2,7 +2,7 @@ export default function getResponseFromAPI() {
   const p = new Promise(((resolve, reject) => {
     const success = true;
     if (success) {
-      resolve(success);
+      resolve('success');
     } else {
       reject('failed');
     }
