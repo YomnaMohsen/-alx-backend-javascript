c52aaf68fa4cb1025f396788ae676c57225cc821
YomnaMohsen <ymn_mohsen@yahoo.com>
2024-11-16 21:51:31 +0200

    fix

--- 0x05-Node_JS_basic/full_server/controllers/AppController.js
+++ 0x05-Node_JS_basic/full_server/controllers/AppController.js
@@ -1,8 +1,6 @@
-class AppController{
-    static getHomepage(request, response){
-        response.status(200).send("Hello Holberton School!");
-
-    }
-
+class AppController {
+  static getHomepage(request, response) {
+    response.status(200).send('Hello Holberton School!');
+  }
 }
 module.exports = AppController;
--- 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -1,42 +1,37 @@
 const readDatabase = require('../utils');
 
-class StudentsController{
-    static getAllStudents(request, response){
-        readDatabase(process.argv[2])
-        .then((data)=>{
-            response.status(200);
-            response.write("This is the list of our students\n");
-            Object.keys(data).forEach(element => {
-            response.write(`Number of students in ${element}: ${data[element].length}. List: ${data[element].join(', ')}\n`);       
-            });
-            response.end();  
-        })
-        .catch((err) =>{
-            response.status(500).send(err.message);
-            response.end();
-       });
+class StudentsController {
+  static getAllStudents(request, response) {
+    readDatabase(process.argv[2])
+      .then((data) => {
+        response.status(200);
+        response.write('This is the list of our students\n');
+        Object.keys(data).forEach((element) => {
+          response.write(`Number of students in ${element}: ${data[element].length}. List: ${data[element].join(', ')}\n`);
+        });
+        response.end();
+      })
+      .catch((err) => {
+        response.status(500).send(err.message);
+        response.end();
+      });
+  }
 
-    }
-
-    static getAllStudentsByMajor(request, response){
-        const field = request.params.major;
-        readDatabase(process.argv[2])
-        .then((data)=>{
-            if(data[field]){
-                response.status(200).send(`List: ${data[field].join(', ')}`);
-            }
-            else{
-                response.status(500).send(`Major parameter must be CS or SWE`);
-            }    
-                
-            })
-            .catch((err) =>{
-                response.status(500).send(err.message);
-                response.end();
-            });
-
-    }
+  static getAllStudentsByMajor(request, response) {
+    const field = request.params.major;
+    readDatabase(process.argv[2])
+      .then((data) => {
+        if (data[field]) {
+          response.status(200).send(`List: ${data[field].join(', ')}`);
+        } else {
+          response.status(500).send('Major parameter must be CS or SWE');
+        }
+      })
+      .catch((err) => {
+        response.status(500).send(err.message);
+        response.end();
+      });
+  }
 }
-
 
 module.exports = StudentsController;
