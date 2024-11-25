796f4b882aefbce189f2dfea83f6cba1d630f213
YomnaMohsen <ymn_mohsen@yahoo.com>
2024-11-25 23:09:10 +0200

    fix

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,9 +1,9 @@
+/* eslint-disable no-undef */
 console.log('Welcome to Holberton School, what is your name?');
 
 process.stdin.on('readable', () => {
-  const chunk = process.stdin.read();
-
-  while ((chunk) !== null) {
+  // eslint-disable-next-line no-cond-assign
+  while ((chunk = process.stdin.read()) !== null) {
     process.stdout.write(`Your name is: ${chunk}`);
   }
 });
