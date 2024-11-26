51832e5b37b55121200de0273be5a97845ac0213
YomnaMohsen <ymn_mohsen@yahoo.com>
2024-11-26 20:44:27 +0200

    try1

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -4,66 +4,55 @@ const fs = require('fs');
 function countStudents(path) {
   return new Promise((resolve, reject) => {
     // eslint-disable-next-line consistent-return
-    fs.readFile(path, (error, dataBuffer) => {
-      if (error) {
+    fs.readFile(path, (err, data) => {
+      if (err) {
         return reject();
       }
-      const data = dataBuffer.toString().split('\n');
+      const datarows = data.toString().split('\n');
+      const arr = {};
       let count = 0;
-      const fields = {};
-
-      const firstnameIndex = data[0].split(',').indexOf('firstname');
-      const fieldIndex = data[0].split(',').indexOf('field');
-      // eslint-disable-next-line no-plusplus
-      for (let i = 1; i < data.length; i++) {
+      const nameind = datarows[0].split(',').indexOf('firstname');
+      const fieldind = datarows[0].split(',').indexOf('field');
+      for (let i = 1; i < datarows.length; i += 1) {
         // eslint-disable-next-line no-continue
-        if (data[i] === '') continue;
-        // eslint-disable-next-line no-plusplus
-        count++;
-        const row = data[i].split(',');
-        if (fields[row[fieldIndex]]) {
-          fields[row[fieldIndex]].push(row[firstnameIndex]);
+        if (datarows[i] === '') continue;
+        const record = datarows[i].split(',');
+        count += 1;
+
+        if (arr[record[fieldind]]) {
+          arr[record[fieldind]].push(record[nameind]);
         } else {
-          fields[row[fieldIndex]] = [row[firstnameIndex]];
+          arr[record[fieldind]] = [record[nameind]];
         }
       }
-      let studentsData = 'This is the list of our students\n';
-      studentsData += `Number of students: ${count}\n`;
-
+      let stdstr = 'This is the list of our students\n';
+      stdstr = `Number of students: ${count}\n`;
       // eslint-disable-next-line guard-for-in
-      for (const field in fields) {
-        studentsData += `Number of students in ${field}: ${
-          fields[field].length
-        }. List: ${fields[
-          field
-          // eslint-disable-next-line comma-dangle
-        ].join(', ')}\n`;
+      for (const key in arr) {
+        stdstr += `Number of students in ${key}: ${arr[key].length}. List: ${arr[key].join(', ')}\n`;
       }
-      resolve(studentsData.slice(0, -1));
+      resolve(stdstr.slice(0, -1));
     });
   }).catch(() => {
     throw new Error('Cannot load the database');
   });
 }
-const app = http
-  // eslint-disable-next-line consistent-return
-  .createServer((req, res) => {
-    if (req.url === '/') {
-      res.setHeader('Content-Type', 'text/plain');
-      return res.end('Hello Holberton School!');
-    }
-    if (req.url === '/students') {
-      const path = process.argv[2];
-      countStudents(path)
-        .then((data) => {
-          res.setHeader('Content-Type', 'text/plain');
-          res.end(data);
-        })
-        .catch((error) => {
-          res.setHeader('Content-Type', 'text/plain');
-          res.end(`This is the list of our students\n${error.message}`);
-        });
-    }
-  })
-  .listen(1245);
+// eslint-disable-next-line consistent-return
+const app = http.createServer((req, res) => {
+  if (req.url === '/') {
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello Holberton School!');
+  } else if (req.url === '/students') {
+    countStudents(process.argv[2])
+      .then((data) => {
+        res.setHeader('Content-Type', 'text/plain');
+        res.end(data);
+      })
+      .catch((error) => {
+        res.setHeader('Content-Type', 'text/plain');
+        res.end(`This is the list of our students\n'${error.message}`);
+      });
+  }
+});
+app.listen(1245);
 module.exports = app;
