e31aa6ad29e62e16cb2986875a6df542c60dc3f2
YomnaMohsen <ymn_mohsen@yahoo.com>
2024-11-26 14:29:37 +0200

    t7

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,8 +1,41 @@
 const express = require('express');
-
-const countStudents = require('./3-read_file_async');
+const fs = require('fs');
 
 const app = express();
+
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    // eslint-disable-next-line consistent-return
+    fs.readFile(path, 'utf-8', (err, data) => {
+      if (err) {
+        reject(err);
+      }
+      const datarows = data.split('\n');
+      const arr = {};
+      let count = 0;
+      const nameind = datarows[0].split(',').indexOf('firstname');
+      const fieldind = datarows[0].split(',').indexOf('field');
+      for (let i = 1; i < datarows.length; i += 1) {
+        // eslint-disable-next-line no-continue
+        if (datarows[i] === '') continue;
+        const record = datarows[i].split(',');
+        count += 1;
+
+        if (arr[record[fieldind]]) {
+          arr[record[fieldind]].push(record[nameind]);
+        } else {
+          arr[record[fieldind]] = [record[nameind]];
+        }
+      }
+      let stdstr = `Number of students: ${count}\n`;
+      // eslint-disable-next-line guard-for-in
+      for (const key in arr) {
+        stdstr += `Number of students in ${key}: ${arr[key].length}. List: ${arr[key].join(', ')}\n`;
+      }
+      resolve(stdstr.slice(0, -1));
+    });
+  });
+}
 
 app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
@@ -11,7 +44,7 @@ app.get('/students', (req, res) => {
   res.write('This is the list of our students\n');
   countStudents(process.argv[2])
     .then((data) => { res.end(data); })
-    .catch((error) => { res.end(error); });
+    .catch(() => { res.end('Cannot load the database'); });
 });
 
 app.listen(1245);
