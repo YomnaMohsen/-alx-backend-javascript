fb07c24079360c3522e996d9b56f9a9fa71c3cdb
YomnaMohsen <ymn_mohsen@yahoo.com>
2024-11-27 21:40:06 +0200

    fix

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -4,8 +4,8 @@ const getPaymentTokenFromAPI = require('./6-payment_token');
 
 describe('getPaymentTokenFromAPI', function(){
  
-    it ('promise always true', function() {
-        getPaymentTokenFromAPI(true).then((response) => {
+    it ('promise always true', () => {
+        return getPaymentTokenFromAPI(true).then((response) => {
         assert.deepEqual(response, {data: 'Successfulresponse from the API'} );
         });  
     });
