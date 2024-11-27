9b1b97cce398c183ac951abdb7eaac7f45155ef6
YomnaMohsen <ymn_mohsen@yahoo.com>
2024-11-27 21:25:42 +0200

    fix

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -2,13 +2,12 @@ const sinon = require('sinon');
 const Utils = require('./utils');
 const sendPaymentRequestToApi = require('./3-payment');
 
-describe('sendPaymentRequestToApi', function(){
+describe('sendPaymentRequestToApi', () => {
     it ('should produce correct output', () =>{
         const spy = sinon.spy(Utils, "calculateNumber");
         sendPaymentRequestToApi(100, 20);
         sinon.assert.calledWithMatch(spy, 'SUM', 100, 20);
         spy.restore();
     });
-
 });
 
