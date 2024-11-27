643433f577613324bae19a32c357a707a489aeff
YomnaMohsen <ymn_mohsen@yahoo.com>
2024-11-27 21:38:54 +0200

    t6

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -4,10 +4,9 @@ const getPaymentTokenFromAPI = require('./6-payment_token');
 
 describe('getPaymentTokenFromAPI', function(){
  
-    it ('promise always true', function(done) {
+    it ('promise always true', function() {
         getPaymentTokenFromAPI(true).then((response) => {
         assert.deepEqual(response, {data: 'Successfulresponse from the API'} );
-        done();
         });  
     });
 });
