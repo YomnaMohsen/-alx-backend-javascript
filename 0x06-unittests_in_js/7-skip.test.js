df1a69dbda85c164e70532ed17ec343f255ea4a2
YomnaMohsen <ymn_mohsen@yahoo.com>
2024-11-27 21:48:25 +0200

    skip

--- 0x06-unittests_in_js/7-skip.test.js
+++ 0x06-unittests_in_js/7-skip.test.js
@@ -1,6 +1,6 @@
 const { expect } = require('chai');
 
-describe('Testing numbers', () => {
+describe.skip('Testing numbers', () => {
   it('1 is equal to 1', () => {
     expect(1 === 1).to.be.true;
   });
